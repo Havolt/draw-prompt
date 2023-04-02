c1da4ed587cfa8b11c7345b7de8013c56a4eca1e
markf <markfitz815@gmail.com>
2023-04-02 21:25:16 +0100

    add timer percent for progress fill bar

--- frontend/src/components/TimerBar.tsx
+++ frontend/src/components/TimerBar.tsx
@@ -4,11 +4,17 @@ import "./TimerBar.css";
 
 function TimerBar() {
   const [remainingTime, setRemainingTime] = useState(0);
+  const [timerPercent, setTimerPercent] = useState(0);
 
   return (
     <div className="timer-bar">
       <div className="timer-bar__time">{remainingTime}</div>
-      <div className="timer-bar__progress" />
+      <div className="timer-bar__progress">
+        <div
+          className="timer-bar__progress-fill"
+          style={{ width: `${timerPercent}%` }}
+        />
+      </div>
     </div>
   );
 }
